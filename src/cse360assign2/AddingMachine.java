e147a7880e06634279e4e56a3d6ea317e8e02f7b
amowe <amowe@DESKTOP-OSAKOKN>
2019-10-08 08:54:24 -0700

    initial version

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,49 @@
+//Angelique Mowery
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	/**
+	 *This method stores the initial values for variable used in
+	 *the AddingMachine
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 *This method 
+	 * @return total value after calculations
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * This method adds a value to the total 
+	 * @param value is the value being added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * This method subtracts a value from the total
+	 * @param value is value being subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * This method returns the Calculations in string format
+	 * (i.e) "0 + 5 - 3 + 2" 
+	 */
+	public String toString () {
+		return "";
+	}
+	/**
+	 * This method clears the string that is returned by the toString() method
+	 */
+	public void clear() {
+	
+	}
+}

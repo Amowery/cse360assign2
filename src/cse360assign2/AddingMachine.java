7a2ea99f43e96dd58874a6261886f720ce5cd39e
amowe <amowe@DESKTOP-OSAKOKN>
2019-10-08 09:19:00 -0700

    modified/second version

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -5,45 +5,52 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private StringBuffer Calc; 
 	/**
 	 *This method stores the initial values for variable used in
-	 *the AddingMachine
+	 *the AddingMachine. StringBuffer used in order to empty string. 
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		Calc = new StringBuffer("0"); 
 	}
 	/**
 	 *This method 
 	 * @return total value after calculations
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	/**
-	 * This method adds a value to the total 
+	 * This method adds a value to the total and appends " + value " to string 
 	 * @param value is the value being added to the total
 	 */
 	public void add (int value) {
+		total += value; 
+		Calc.append(" + "+ value);
 		
 	}
 	/**
-	 * This method subtracts a value from the total
+	 * This method subtracts a value from the total and append " - value" to string
 	 * @param value is value being subtracted from total
 	 */
 	public void subtract (int value) {
-		
+		total = total - value; 
+		Calc.append(" - "+ value);
 	}
 	/**
 	 * This method returns the Calculations in string format
 	 * (i.e) "0 + 5 - 3 + 2" 
 	 */
 	public String toString () {
-		return "";
+		return Calc.toString();
 	}
 	/**
 	 * This method clears the string that is returned by the toString() method
+	 * by resetting stringBuffer to original value and total to original value
 	 */
 	public void clear() {
-	
+		Calc.delete(0, Calc.length());
+		total = 0; 
 	}
 }
